91422801d32d9316fa66b891841c04f27f5a6dfb
knetto <corneschipper2003@gmail.com>
2025-01-17 11:51:43 +0100

    text responsive

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,73 @@
-const open = document.querySelector('.container');
-		const close = document.querySelector('.close');
-		var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
-		open.addEventListener('click', () => {
-			if (tl.reversed()) {
-				tl.play();
-			} else {
-				tl.to('nav', { right: 0, duration: 0 })
-					.to('nav', { height: '100vh' }, '-=.1')
-					.to('nav ul li a', { opacity: 1, pointerEvents: 'all', stagger: .2 }, '-=.8')
-					.to('.close', { opacity: 1, pointerEvents: 'all' }, "-=.8")
-					.to('nav h2', { opacity: 1 }, '-=1');
-			}
-		});
+// Scroll Prevention Logic
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1, 32: 1, 33: 1, 34: 1, 35: 1, 36: 1 };
 
-		close.addEventListener('click', () => {
-			tl.reverse();
-		});
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+var supportsPassive = false;
+try {
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      },
+    })
+  );
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+
+// Disable and Enable Scroll Functions
+function disableScroll() {
+  window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+  window.removeEventListener("DOMMouseScroll", preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener("touchmove", preventDefault, wheelOpt);
+  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+// GSAP Animation Logic
+const open = document.querySelector(".container");
+const close = document.querySelector(".close");
+var tl = gsap.timeline({ defaults: { duration: 1, ease: "expo.inOut" } });
+
+open.addEventListener("click", () => {
+  if (tl.reversed()) {
+    tl.play();
+  } else {
+    // Disable scrolling when the menu is open
+    disableScroll();
+
+    tl.to("nav", { right: 0, duration: 0 })
+      .to("nav", { height: "100vh" }, "-=.1")
+      .to("nav ul li a", { opacity: 1, pointerEvents: "all", stagger: 0.2 }, "-=.8")
+      .to(".close", { opacity: 1, pointerEvents: "all" }, "-=.8")
+      .to("nav h2", { opacity: 1 }, "-=1");
+  }
+});
+
+close.addEventListener("click", () => {
+  tl.reverse();
+
+  // Enable scrolling when the menu is closed
+  tl.eventCallback("onReverseComplete", () => {
+    enableScroll();
+  });
+});
