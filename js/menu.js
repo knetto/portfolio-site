f4c028429d4846a814f986d8d487eb02899209f8
knetto <corneschipper2003@gmail.com>
2025-01-24 20:40:32 +0100

    test

--- js/menu.js
+++ js/menu.js
@@ -1,47 +1,47 @@
-// Scroll Prevention Logic
-var keys = { 37: 1, 38: 1, 39: 1, 40: 1, 32: 1, 33: 1, 34: 1, 35: 1, 36: 1 };
+// Scroll Prevention Logic Removed
+// var keys = { 37: 1, 38: 1, 39: 1, 40: 1, 32: 1, 33: 1, 34: 1, 35: 1, 36: 1 };
 
-function preventDefault(e) {
-  e.preventDefault();
-}
+// function preventDefault(e) {
+//   e.preventDefault();
+// }
 
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
+// function preventDefaultForScrollKeys(e) {
+//   if (keys[e.keyCode]) {
+//     preventDefault(e);
+//     return false;
+//   }
+// }
 
-var supportsPassive = false;
-try {
-  window.addEventListener(
-    "test",
-    null,
-    Object.defineProperty({}, "passive", {
-      get: function () {
-        supportsPassive = true;
-      },
-    })
-  );
-} catch (e) {}
+// var supportsPassive = false;
+// try {
+//   window.addEventListener(
+//     "test",
+//     null,
+//     Object.defineProperty({}, "passive", {
+//       get: function () {
+//         supportsPassive = true;
+//       },
+//     })
+//   );
+// } catch (e) {}
 
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+// var wheelOpt = supportsPassive ? { passive: false } : false;
+// var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
 
-// Disable and Enable Scroll Functions
-function disableScroll() {
-  window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
-  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
-}
+// Disable and Enable Scroll Functions Removed
+// function disableScroll() {
+//   window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+//   window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+//   window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+//   window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+// }
 
-function enableScroll() {
-  window.removeEventListener("DOMMouseScroll", preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-  window.removeEventListener("touchmove", preventDefault, wheelOpt);
-  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
-}
+// function enableScroll() {
+//   window.removeEventListener("DOMMouseScroll", preventDefault, false);
+//   window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+//   window.removeEventListener("touchmove", preventDefault, wheelOpt);
+//   window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+// }
 
 // GSAP Animation Logic
 const open = document.querySelector(".container");
@@ -52,9 +52,7 @@ open.addEventListener("click", () => {
   if (tl.reversed()) {
     tl.play();
   } else {
-    // Disable scrolling when the menu is open
-    disableScroll();
-
+    // Removed disableScroll() as it's no longer needed
     tl.to("nav", { right: 0, duration: 0 })
       .to("nav", { height: "100vh" }, "-=.1")
       .to("nav ul li a", { opacity: 1, pointerEvents: "all", stagger: 0.2 }, "-=.8")
@@ -66,8 +64,5 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
   tl.reverse();
 
-  // Enable scrolling when the menu is closed
-  tl.eventCallback("onReverseComplete", () => {
-    enableScroll();
-  });
+  // Removed enableScroll() as it's no longer needed
 });
